9e99bab45004c2ee9b81cc8c39aeca7ff56ee1b5
brightnanon <112468101+brightnanon@users.noreply.github.com>
2023-06-08 16:05:49 +0700

    Update bright.js

--- bright.js
+++ bright.js
@@ -1,5 +1,5 @@
 let num = [3, 10, 12, 13, 15, 28, 30];
-//const num =  15;
+let b = num.filter(bangboom);
 
 function bangboom(num){
 if (num % 3 == 0 && num % 5 != 0 ){
@@ -15,14 +15,3 @@ else{
     console.log('MISS');
 }
 }
-console.log(num.filter(bangboom));
-
-//let a = arr.filter(num => num % 3 == 0 && num % 5 != 0);
-//let b = arr.filter(num => num % 5 == 0 && num % 3 != 0);
-//let c = arr.filter(num => num % 3 == 0 && num % 5 == 0);
-//let d = arr.filter(num => num % 3 != 0 && num % 5 != 0);
-
-//a.forEach(item => {console.log(a + '= BANG')} );
-//b.forEach(item => {console.log(b +'= BOOM')} );
-//c.forEach(item => {console.log(c + '= BANGBOOM')} );
-//d.forEach(item => {console.log(d + '= MISS')} );
